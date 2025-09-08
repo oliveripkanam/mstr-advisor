510aa41289e1d819c3fd5c8fa1420e9f7b061c2b
Oliver Ip <72657205+MrOliverYt@users.noreply.github.com>
2025-09-08 17:51:34 +0800

    fix(pages): hardcode basePath '/mstr-advisor' to resolve _next 404s

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
 const nextConfig = {
   output: 'export',
   distDir: 'out',
-  basePath: basePath || undefined,
-  assetPrefix: basePath || undefined,
+  basePath: '/mstr-advisor',
 };
 
 module.exports = nextConfig;
